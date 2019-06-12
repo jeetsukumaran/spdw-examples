2a6805098b745785eee6dfa23edee7c11eabe094
Jeet Sukumaran <jeetsukumaran@gmail.com>
2019-06-13 07:54:11 +1000

    Update for new

--- bpp-with-bd-trees/bin/run.sh
+++ bpp-with-bd-trees/bin/run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-python3 ../../../../2019-06-11-species-delimitation-anu-practicals/spdw/bin/spdw-sim-bdtrees.py --num-tips 5 --birth-rate 0.1 bd
-python3 ../../../../2019-06-11-species-delimitation-anu-practicals/spdw/bin/spdw-build-bpp-jobs.py --num-individuals-per-population 2 --num-loci-per-individual 5 --num-characters-per-locus 1000 -f newick --mutation 0.0001 bd.trees
+spdw-sim-bdtrees.py --num-tips 5 --birth-rate 0.1 bd
+spdw-build-bpp-jobs.py --num-individuals-per-population 2 --num-loci-per-individual 5 --num-characters-per-locus 1000 -f newick --mutation 0.0001 bd.trees
 
