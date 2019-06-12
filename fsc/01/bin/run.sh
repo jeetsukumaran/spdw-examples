6fe8ac4929582dd432fca6dbeff3a590ad81c396
Jeet Sukumaran <jeetsukumaran@gmail.com>
2019-06-13 07:55:30 +1000

    Update for new

--- fsc/01/bin/run.sh
+++ fsc/01/bin/run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-python3 ../../../../spdw/bin/spdw-sim-bdtrees.py --num-reps 1 --num-tips 5 --birth-rate 0.1 --scale 1000 bd
-python3 ../../../../spdw/bin/spdw-build-fsc26-run.py -k 2 -N 1000 --num-loci 4 bd.trees
+spdw-sim-bdtrees.py --num-reps 1 --num-tips 5 --birth-rate 0.1 --scale 1000 bd
+spdw-build-fsc26-run.py -k 2 -N 1000 --num-loci 4 bd.trees
 fsc26 -i fscrun.par -n 1 -T
